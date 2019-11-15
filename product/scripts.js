e3caff1c50e1aa7f713f4a6b7447878a701c5f99
Roseakhonya <akhonyarose@gmail.com>
2019-11-15 07:48:26 +0300

    add alert message

--- product/scripts.js
+++ product/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   var sanitary = $("#sanitary:checked").val();
 
     $("#contact-form").submit (function(event) {
-      if ($("input#name").val() && $("input#number").val()) {
+      if ($("input#name").val() == name && $("input#number").val()) {
         alert("Hey " + name + ", thank you for contacting us, your request for " 
         +  sanitary  + "  will receive an SMS notifiction soon for the pickup"
         );
